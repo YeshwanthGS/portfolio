07f6795bbc47b2e7b5b8d8ae58377ccef52d1175
YeshwanthGS <yeshu.mrcet2019@gmail.com>
2023-08-27 11:57:29 +0530

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Tejaswan - My Portfolio";
+export const SITE_DESCRIPTION =
+  "This website is about me. Hi this is Tejaswan, I am a web developer";
+export const SITE_URL = "https://tejaswan.me";
